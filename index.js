b8de81ccf233ad68cebadf46c5b332da0909e3d2
antoniotex <acm.skt@gmail.com>
2018-01-30 15:04:07 -0200

    Criado rotas de atualizacao, exclusao e selecao

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const server = restify.createServer({
 });
 
 const knex = require('knex')({
-  client: 'mysql',
-  connection: {
-    host : '127.0.0.1',
-    user : 'root',
-    password : '',
-    database : 'db'
-  }
-});
+    client: 'mysql',
+    connection: {
+      host : '127.0.0.1',
+      user : 'root',
+      password : '',
+      database : 'db'
+    }
+  });
 
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
@@ -24,22 +24,64 @@ server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
 
-server.get('/', function (req, res, next) {
+// rotas REST
 
-  knex('rest').then((dados)=>{
-    res.send(dados);
-  }, next);
+server.get('/', (req, res, next) => {
 
-  return next();
+    knex('rest').then((dados) => {
+        res.send(dados);
+    }, next)
+
 });
 
-server.post('/create', function (req, res, next) {
+server.post('/create', (req, res, next) => {
 
-  knex('rest')
-  .insert(req.body)
-  .then((dados)=>{
-    res.send(dados);
-  }, next);
+    knex('rest')
+        .insert(req.body)
+        .then((dados) => {
+            res.send(dados);
+        }, next)
 
-  return next();
+});
+
+server.get('/show/:id', (req, res, next) => {
+
+    const { id } = req.params;
+
+    knex('rest')
+        .where('id', id)
+        .first()
+        .then((dados) => {
+            if(!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
+            res.send(dados);
+        }, next)
+
+});
+
+server.put('/update/:id', (req, res, next) => {
+
+    const { id } = req.params;
+
+    knex('rest')
+        .where('id', id)
+        .update(req.body)
+        .then((dados) => {
+            if(!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
+            res.send('dados atualizados');
+        }, next)
+
+});
+
+server.del('/delete/:id', (req, res, next) => {
+
+    const { id } = req.params;
+
+    knex('rest')
+        .where('id', id)
+        .delete()
+        .then((dados) => {
+            if(!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
+            res.send('dados excluidos');
+        }, next)
+
 });
